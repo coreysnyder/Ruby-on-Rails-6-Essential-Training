e6fcc203277bea424a0e1387de7dd561a0dee39c
Corey Snyder <corey.snyder@joinroot.com>
2021-12-27 15:24:30 -0500

    Adds Subjects and Pages controllers via command line: rails generate controller Pages index show new edit delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  get 'pages/index'
+  get 'pages/show'
+  get 'pages/new'
+  get 'pages/edit'
+  get 'pages/delete'
+  get 'subjects/index'
+  get 'subjects/show'
+  get 'subjects/new'
+  get 'subjects/edit'
+  get 'subjects/delete'
+  root 'demo#index'
+
+  # get 'demo/index'
+  get 'demo/hello'
+  get 'demo/contact'
+
+  # Default Route
+  get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
