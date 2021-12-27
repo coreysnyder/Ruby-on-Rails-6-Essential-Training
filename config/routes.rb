1aff352c30d8fc5d0b4caee1ec6d6a046f7e2fdc
Corey Snyder <corey.snyder@joinroot.com>
2021-12-27 15:58:25 -0500

    Adds controllers and views for index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,33 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/delete'
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
   root 'demo#index'
+
 
   # get 'demo/index'
   get 'demo/hello'
   get 'demo/contact'
+
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
 
   # Default Route
   get ':controller(/:action(/:id))'
