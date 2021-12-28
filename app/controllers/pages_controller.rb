d6313b8afffc91dc6a5b1f2ed452ae68c1f0e933
Corey Snyder <corey.snyder@joinroot.com>
2021-12-28 10:55:41 -0500

    Adds View/Controller for Pages create/edit action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,13 +8,32 @@ class PagesController < ApplicationController
   end
 
   def new
+    # Instantiate a new Page with a default visibility of true
+    @page = Page.new(:visible => true)
   end
   def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      # If we successfully create the new page
+      redirect_to(pages_path(@page))
+    else
+      render('new')
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
   def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      # If we successfully edit the new page
+      redirect_to(pages_path(@page))
+    else
+      render('new')
+    end
   end
 
   def delete
