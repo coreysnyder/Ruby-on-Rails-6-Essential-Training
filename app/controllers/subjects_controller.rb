f68486dd6572c3f571efe9807387ff36d19ad47a
Corey Snyder <corey.snyder@joinroot.com>
2021-12-27 21:57:47 -0500

    Adds controllers and views for create and edit actions

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,17 +8,44 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new(:visible => true)
   end
   def create
+    # Instantiate a new object using form params
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      # if save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+      # if save fails, redisplay the form so the user can fix problems
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
   def update
+    @subject = Subject.find(params[:id])
+
+    if @subject.update(subject_params)
+      # if save succeeds, redirect to the index action
+      redirect_to(subject_path(@subject))
+    else
+      # if save fails, redisplay the form so the user can fix problems
+      render('edit')
+    end
+
   end
 
   def delete
   end
   def destroy
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit(:name, :visible, :position)
   end
 end
